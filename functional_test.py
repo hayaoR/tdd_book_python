1b09ccb66c1719ba7fb897f41812eecdde1fa1b2
koki honda <kokihonda@am.ics.keio.ac.jp>
2021-01-05 13:32:59 +0900

    Front page HTML now generated from a template

--- functional_test.py
+++ functional_test.py
@@ -22,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_elements_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -34,9 +34,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_elements_by_id('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock features' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock features' for row in rows), 'Now to-do item did not appear in table')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
