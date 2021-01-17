ced2138b7a73226607a434b71f27baad6a8d1e55
koki honda <kokihonda@am.ics.keio.ac.jp>
2021-01-17 10:37:12 +0900

    rename all item input ids and names. still broken

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -14,7 +14,7 @@ class NewVisitorTest(FuntionalTest):
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box() 
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -28,7 +28,7 @@ class NewVisitorTest(FuntionalTest):
 
         # There is still a text box inviting her to add another item.
         # She enters 'Use peacock feathers to make a fly'
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box() 
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
@@ -52,7 +52,7 @@ class NewVisitorTest(FuntionalTest):
     def test_multiple_users_can_start_lists_at_diffrent_urls(self):
         # Edith starts a new to-do app
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box() 
         inputbox.send_keys('Buy peacock features')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock features')
@@ -77,7 +77,7 @@ class NewVisitorTest(FuntionalTest):
 
         # Francis starts a new list by entering a new item He
         # is less interesting than Edith...
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box() 
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
