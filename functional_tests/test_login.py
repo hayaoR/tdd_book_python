7785a6479750547712be5853945d7cf356e44ef0
koki honda <kokihonda@am.ics.keio.ac.jp>
2021-01-23 10:03:51 +0900

    loginとその認証機能の実装

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -40,7 +40,7 @@ class LoginTest(FuntionalTest):
 
         # she is logged in!
         self.wait_for(
-            lambda:self.browser.find_element_by_link_text('Log out') 
+            lambda:self.browser.find_element_by_link_text('Log out')
         )
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(TEST_EMAIL, navbar.text)
--- superlists/settings.py
+++ superlists/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'accounts.User'
 
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -149,3 +153,9 @@ LOGGING = {
     },
     'root': {'level': 'INFO'},
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'tddwithpython@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
