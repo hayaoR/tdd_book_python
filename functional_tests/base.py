ef0896c6845006137d3e1d6054892f160c250d32
koki honda <kokihonda@am.ics.keio.ac.jp>
2021-01-26 10:16:18 +0900

    staging server上でpre_authenticated_sessionを動作させるための変更

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,6 +5,8 @@ from selenium.common.exceptions import WebDriverException
 import time
 import os
 
+from .server_tools import reset_database
+
 MAX_WAIT = 10
 
 def wait(fn):
@@ -19,13 +21,13 @@ def wait(fn):
                 time.sleep(0.5)
     return modified_fn
 
-
 class FuntionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
     
     def tearDown(self):
         self.browser.quit()
@@ -45,12 +47,12 @@ class FuntionalTest(StaticLiveServerTestCase):
         
     @wait
     def wait_to_be_logged_in(self, email):
-        self.browser.find_element_by_link_text('Log out') 
+        self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
     
     @wait
     def wait_to_be_logged_out(self, email):
-        self.browser.find_element_by_name('email') 
+        self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
