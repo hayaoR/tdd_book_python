71c659fa2bf2f7da1ac2e9a79ac4874951fcafbd
koki honda <kokihonda@am.ics.keio.ac.jp>
2021-01-27 10:01:43 +0900

    base.py にadd_list_item関数を実装し、test_list_item_validattionに反映した

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -17,10 +17,9 @@ class LayoutAndStylingTest(FuntionalTest):
 
         # She starts a new list and sees the input is nicely
         # centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
+        self.add_list_item('testing')
         self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.get_item_input_box() 
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -49,8 +49,7 @@ class ItemValidation(FuntionalTest):
     def test_cannot_add_duplicate_items(self):
         # Edith goese to the home page and starts a new list
         self.browser.get(self.live_server_url)
-        self.get_item_input_box().send_keys('Buy wellies')
-        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.add_list_item('Buy wellies')
         self.wait_for_row_in_list_table('1: Buy wellies')
 
         # She accidentally tries to enter a duplicat item
@@ -66,8 +65,7 @@ class ItemValidation(FuntionalTest):
     def test_error_message_are_cleared_on_input(self):
         # Edith starts a list and cause a validation error
         self.browser.get(self.live_server_url)
-        self.get_item_input_box().send_keys('Banter too thick')
-        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.add_list_item('Banter too thick')
         self.wait_for_row_in_list_table('1: Banter too thick')
         self.get_item_input_box().send_keys('Banter too thick')
         self.get_item_input_box().send_keys(Keys.ENTER)
