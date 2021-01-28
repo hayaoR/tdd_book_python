da0133bce680b91442a58384f831bc6e17b98023
koki honda <kokihonda@am.ics.keio.ac.jp>
2021-01-29 08:54:47 +0900

    tidy up: remove deplicate tests, view_list2 -> view_list etc.

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -18,13 +18,6 @@ class ItemFormTest(TestCase):
             form.errors['text'],
             [EMPTY_ITEM_ERROR]
         )
-    def test_form_save_handles_saving_to_a_list(self):
-        list_ = List.objects.create()
-        form = ItemForm(data={'text': 'do me'})
-        new_item = form.save(for_list=list_)
-        self.assertEqual(new_item, Item.objects.first())
-        self.assertEqual(new_item.text, 'do me')
-        self.assertEqual(new_item.list, list_)
 
     def test_form_validation_for_duplicate_items(self):
         list_ = List.objects.create()
