4b04b3e4e7f8afa52c6db3f81390ebe62a38c22c
koki honda <kokihonda@am.ics.keio.ac.jp>
2021-02-02 10:39:05 +0900

    New FT for sharing, move session creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,7 +1,12 @@
+from .server_tools import create_session_on_server
+from .management.commands.create_session import create_pre_authenticated_session
+
+from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+
 
 import time
 import os
@@ -32,6 +37,20 @@ class FuntionalTest(StaticLiveServerTestCase):
     
     def tearDown(self):
         self.browser.quit()
+
+    def create_pre_authenticated_session(self, email):
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+        ## to set a cookie we need to first visit the domain,
+        ## 404 pages load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_url/") 
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/',
+        ))
     
     @wait
     def wait_for(self, fn):
--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -3,27 +3,12 @@ from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
 from django.contrib.sessions.backends.db import SessionStore
 from .base import FuntionalTest
 User = get_user_model()
-from .server_tools import create_session_on_server
-from .management.commands.create_session import create_pre_authenticated_session
 
 class MyListTest(FuntionalTest):
-    def create_pre_authentivated_session(self, email):
-        if self.staging_server:
-            session_key = create_session_on_server(self.staging_server, email)
-        else:
-            session_key = create_pre_authenticated_session(email)
-        ## to set a cookie we need to first visit the domain,
-        ## 404 pages load the quickest!
-        self.browser.get(self.live_server_url + "/404_no_such_url/") 
-        self.browser.add_cookie(dict(
-            name=settings.SESSION_COOKIE_NAME,
-            value=session_key,
-            path='/',
-        ))
 
     def test_logged_in_users_lists_are_saved_as_mylist(self):
         # Edith is a logged in user
-        self.create_pre_authentivated_session('edith@example.com')
+        self.create_pre_authenticated_session('edith@example.com')
 
         # She goes to the home page and starts a list
         self.browser.get(self.live_server_url)
